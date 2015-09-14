7f1a5aab8e965b6dd18af0c64070ef47250a5821
CharlesAMoss <charlesamoss@yahoo.com>
2015-09-13 19:23:22 -0700

    add comments to main.js

--- js/main.js
+++ js/main.js
@@ -1,22 +1,22 @@
 var findReplace = function(phrase, find, replace) {
 
-    var splitString = phrase.split(/\s*\b\s*/);
-    var punctuation = [",", ".", "!", "?", ";", ":"];
-    var output = "";
-    for (var string of splitString) {
-        if (punctuation.indexOf(string) === -1) {
-            if (find.indexOf(string) !== -1) {
-                output = output + replace + " ";
-            } else {
-                output = output + string + " ";
+    var splitString = phrase.split(/\s*\b\s*/);        // separates 'phrase' into an array of words and punctuation
+    var punctuation = [",", ".", "!", "?", ";", ":"];  // crates an array used match punctuation
+    var output = "";                                   // declares the empty string 'output'
+    for (var string of splitString) {                  // loops through the indexes of 'splitString' as 'string'
+        if (punctuation.indexOf(string) === -1) {      // looks for matches in the 'punctuation'
+            if (find.indexOf(string) !== -1) {         // looks for matches of 'find'
+                output = output + replace + " ";       // adds 'replace' to 'output' and appends a space
+            } else {                                   // if 'find' is not matched
+                output = output + string + " ";        // adds 'string'(word) to 'output' and appends a space
             } // end of if
-        } else {
-            output = output.slice(0, -1) + string + " ";
+        } else {                                       // if 'punctuation' is matched
+            output = output.slice(0, -1) + string + " ";  // removes a preceding space then adds 'string'(punctuation) and appends a space
         } // end of if
     } // end of for
-    var result = output.slice(0, -1);
+    var result = output.slice(0, -1);                  // removes the last space from 'output'
 
-    return result;
+    return result;                                     
 };
 
 
